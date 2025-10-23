9265356d64e42b3eb3c9f6b88f52e8afe68af2bf
fronre <islamhala35@gmail.com>
2025-10-23 20:49:50 +0100

    test fact(1) = return 1

--- tests/test_mathlib.py
+++ tests/test_mathlib.py
@@ -1,5 +1,9 @@
 from src.mathlib import factorial
-def test_0_to_return_1():
-        factorial(0)
-        return 1
 
+def test_0_to_return_1():
+    factorial(0)
+    return 1
+
+def test_1_to_return_1():
+    factorial(1)
+    return 1
