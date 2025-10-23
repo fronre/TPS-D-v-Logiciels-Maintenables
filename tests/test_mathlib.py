2a6fd78b4dae94ffe7814dd4cc6f6f5941b4fd40
fronre <islamhala35@gmail.com>
2025-10-23 21:35:32 +0100

    refactoring

--- tests/test_mathlib.py
+++ tests/test_mathlib.py
@@ -1,9 +1,8 @@
 import pytest
 from src.mathlib import factorial
 
-def test_factorial_2(number):
-     assert factorial(2) == 2
+@pytest.mark.parametrize("n, expected_result", [(0, 1), (1, 1), (2, 2)])
+def test_factorial(n, expected_result):
+    actual_result = factorial(n)
+    assert factorial(n) == expected_result
 
-@pytest.mark.parametrize("number", [0, 1])
-def test_factorial_0_or_1_return_1(number):
-    assert factorial(number) == 1
