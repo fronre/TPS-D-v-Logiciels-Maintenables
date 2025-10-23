237774567d68c8e9a6aabd0c8253b1a5314da6eb
fronre <islamhala35@gmail.com>
2025-10-23 21:54:16 +0100

    refactoring

--- tests/test_mathlib.py
+++ tests/test_mathlib.py
@@ -1,8 +1,17 @@
 import pytest
 from src.mathlib import factorial
 
-@pytest.mark.parametrize("n, expected_result", [(0, 1), (1, 1), (2, 2), (3,6) ])
+import pytest
+from src.mathlib import factorial
+
+@pytest.mark.parametrize("n, expected_result", [
+    (0, 1),
+    (1, 1),
+    (2, 2),
+    (3, 6),
+    (4, 24),
+    (5, 120)
+])
 def test_factorial(n, expected_result):
     actual_result = factorial(n)
-    assert factorial(n) == expected_result
-
+    assert actual_result == expected_result
